39c3c376e7373562b1081626fdf3bf71bdce69ad
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-01-21 09:31:13 -0300

    error create

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,43 +1,47 @@
-import {
-  View,
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native'
-import { useState } from 'react'
-
-import { BackButton } from '../components/BackButton'
-import { Checkbox } from '../components/Checkbox'
+import { useState } from "react";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Feather } from '@expo/vector-icons'
-import colors from 'tailwindcss/colors'
+import colors from "tailwindcss/colors";
 
-const avaiableWeekDays = [
-  'Domingo',
-  'Segunda-feira',
-  'Terça-feira',
-  'Quarta-feira',
-  'Quinta-feira',
-  'Sexta-feira',
-  'Sábado',
-]
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+import { api } from "../lib/axios";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
-  const [weekDays, setWeeekDays] = useState<number[]>([])
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState('');
 
   function handleToggleWeekDay(weekDayIndex: number) {
     if (weekDays.includes(weekDayIndex)) {
-      setWeeekDays((prevState) =>
-        prevState.filter((weekDay) => weekDay !== weekDayIndex),
-      )
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
     } else {
-      setWeeekDays((prevState) => [...prevState, weekDayIndex])
+      setWeekDays(prevState => [...prevState, weekDayIndex])
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+      }
+
+      await api.post('/habits', { title, weekDays })
+      
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito')
     }
   }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView
+      <ScrollView 
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
@@ -51,30 +55,39 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput
+        <TextInput 
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
-          placeholder="Ex.: Exercícios, dormir bem, etc..."
+          placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
         </Text>
 
-        {avaiableWeekDays.map((weekDay, index) => (
-          <Checkbox
-            key={weekDay}
-            title={weekDay}
-            checked={weekDays.includes(index)}
-            onPress={() => handleToggleWeekDay(index)}
-          />
-        ))}
+        {
+          availableWeekDays.map((weekDay, index) => (
+            <Checkbox
+              key={weekDay}
+              title={weekDay}
+              checked={weekDays.includes(index)}
+              onPress={() => handleToggleWeekDay(index)}
+            />
+          ))
+        }
 
         <TouchableOpacity
-          className="w-full h-14 flex-row items-center justify-center bg-green-600 mt-6"
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
-          <Feather name="check" size={20} color={colors.white} />
+          <Feather 
+           name="check"
+           size={20}
+           color={colors.white}
+          />
 
           <Text className="font-semibold text-base text-white ml-2">
             Confirmar
